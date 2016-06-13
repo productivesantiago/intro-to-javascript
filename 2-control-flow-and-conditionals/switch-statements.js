6f77cdd6d7afafad8ec17633f02327cc788de300
DejonGill <dejonjgill@gmail.com>
2016-06-13 15:20:49 -0700

    Into to switch statements complete

--- 2-control-flow-and-conditionals/switch-statements.js
+++ 2-control-flow-and-conditionals/switch-statements.js
@@ -1,4 +1,3 @@
-
 /*
   So did all of those nested if/else if/else statements get a bit hard to read? 
   Well there's another way to write long conditional statements to ease readability.
@@ -14,5 +13,17 @@
   https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/break
 
   From MDN: "The break statement terminates the current loop, switch, or label statement and transfers program control to the statement following the terminated statement."
+
+  And I guess it's a good time to introduce my favorite, the return statement
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/return
+
+  From MDN: "The return statement ends function execution and specifies a value to be returned to the function caller."
+
+  We will get into loops and functions tomorrow, don't worry if you haven't seen them mentioned before.
+
+  For now, just remember that the return statement does two things:
+    1. It ends any conditional checking within that block
+    2. It returns whatever value follows it (if nothing follows return, then undefined is returned)
+
 */
 
